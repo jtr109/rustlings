6205bd1b34b50c3def84cc63eff596d24c84e090
jtr109 <lyp_login@outlook.com>
2019-10-04 11:45:39 +0800

    feat: oct 4 11:45

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,40 +3,9 @@
 
 fn main() {
     let x: i32;
+    x = 1;
     println!("Number {}", x);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Oops! In this exercise, we have a variable binding that we've created on
 // line 5, and we're trying to use it on line 6, but we haven't given it a
